fe3fa54dd5c9baf6c3c6a5423f64e68211688e0a
Adrian Serzysko <boniekadi@gmail.com>
2024-11-19 20:19:14 +0100

    First Tic Tac Toe structure and adding player name

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing((isEditing) => !isEditing);
+  };
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>
+        {isEditing ? "Zapisz" : "Edytuj"}
+      </button>
+    </li>
+  );
+}
